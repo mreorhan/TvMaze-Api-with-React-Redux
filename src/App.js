67f01cc817a8dc5a9c7b6f65875d808e1c40b1ce
mreorhan <videokod7@gmail.com>
2020-01-01 11:43:19 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import './assets/styles/index.scss'
 import { Header, Footer } from './components';
 import Dashboard from './pages/Dashboard';
+import About from './pages/About';
 
 const App = () => {
   return (
     <div className="App">
-      <Header />
-      test {process.env.REACT_APP_TV_MAZE_API_URL_BASE}
-
-      <Dashboard />
-
-      <Footer />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Dashboard />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
