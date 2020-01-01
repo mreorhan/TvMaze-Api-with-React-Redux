f62ff1876f8554bb9a75af78dfcf96acf2d93ec7
mreorhan <videokod7@gmail.com>
2020-01-01 19:43:41 +0300

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,8 +3,10 @@ import React from 'react';
 const About = () => {
 
     return (
-        <div>
-            About
+        <div className="container">
+            <a href="https://github.com/mreorhan/TvMaze-Api-with-React-Redux/tree/master" target="_blank">
+                Fork me on Github
+            </a>
         </div>
     )
 }
