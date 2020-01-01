8f13e6633bc7990f3bce70a8a5431bfb18106207
mreorhan <videokod7@gmail.com>
2020-01-01 15:23:44 +0300

    Add Loader Hoc

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Header from './Header';
 import Footer from './Footer';
 import MovieList from './MovieList';
+import Loader from './Loader';
 
 export {
     Header,
     Footer,
-    MovieList
+    MovieList,
+    Loader
 }
