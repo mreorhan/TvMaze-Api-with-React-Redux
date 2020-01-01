0ce423b27662aa4e2465b2b3445a85cf5f66cf45
mreorhan <videokod7@gmail.com>
2020-01-01 11:42:50 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header>
             Header Section
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+            </ul>
         </header>
     )
 }
