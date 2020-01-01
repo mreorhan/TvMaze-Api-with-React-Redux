75d675a9cb72dfad4fa0c1644ca7be19ef15324c
mreorhan <videokod7@gmail.com>
2020-01-01 20:23:07 +0300

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Header from './Header';
 import Footer from './Footer';
 import MovieList from './MovieList';
 import Loader from './Loader';
+import Error from './Error';
 
 export {
     Header,
     Footer,
     MovieList,
-    Loader
+    Loader,
+    Error
 }
