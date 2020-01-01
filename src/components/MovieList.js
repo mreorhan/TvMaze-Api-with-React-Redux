ebe2d8a18c4452de65c4add6e3acb05166799c42
mreorhan <videokod7@gmail.com>
2020-01-01 20:21:04 +0300

    Update MovieList.js

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import LoaderHOC from '../common/LoaderHoc';
+import withApiHoc from '../common/WithApiHoc';
 import { Link } from 'react-router-dom';
 
 const MovieList = (props) => {
-    const data = props.items.map((movie, index) => {
+    const data = props.items && props.items.map((movie, index) => {
         return (
             <Link to={"/movie/" + movie.show.id} key={index}>
                 <div className="movie-list">
-                    <img src={movie.show.image.medium} className="poster-medium" />
+                    <img alt={movie.show.name} src={movie.show.image.medium} className="poster-medium" />
                     <h3>{movie.show.name}</h3>
                 </div>
             </Link>
         )
     })
-
-    return data;
+    return <div>{data}</div>;
 }
 
 MovieList.propTypes = {
     items: PropTypes.array.isRequired,
 }
 
-export default MovieList;
+export default withApiHoc(MovieList);
