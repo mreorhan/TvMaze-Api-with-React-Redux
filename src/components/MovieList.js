f1c387335a421e4c8e0f93954f4bde6646227417
mreorhan <videokod7@gmail.com>
2020-01-01 19:05:14 +0300

    Update MovieList.js

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import LoaderHOC from '../common/LoaderHoc';
+import { Link } from 'react-router-dom';
 
 const MovieList = (props) => {
-    return (
-        <div>
-            {props.items}
-        </div>
-    )
+    const data = props.items.map((movie, index) => {
+        return (
+            <Link to={"/movie/" + movie.show.id} key={index}>
+                <div className="movie-list">
+                    <img src={movie.show.image.medium} className="poster-medium" />
+                    <h3>{movie.show.name}</h3>
+                </div>
+            </Link>
+        )
+    })
+
+    return data;
 }
 
 MovieList.propTypes = {
