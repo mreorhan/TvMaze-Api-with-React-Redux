bace6ef2385dea70223616edcb2256d4f8102550
mreorhan <videokod7@gmail.com>
2020-01-01 11:42:53 +0300

    Update MovieList.js

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const MovieList = (props) => {
     return (
@@ -6,6 +7,10 @@ const MovieList = (props) => {
             {props.items}
         </div>
     )
+}
+
+MovieList.propTypes = {
+    items: PropTypes.array.isRequired,
 }
 
 export default MovieList;
